6017cd3d8b873f41712ee147a39da77996b588a1
cdechaux <claire.dechaux@ensae.fr>
2024-01-03 10:35:18 +0000

    new search of model - claire

---
+++ claire/search_model.py
@@ -0,0 +1,147 @@
+
+import pandas as pd 
+
+import timeout_decorator
+
+from functools import partial
+
+from hyperopt import fmin, tpe, hp
+
+from sklearnex import patch_sklearn
+patch_sklearn() #Extension to speed calculation (X10-100)
+
+
+
+
+space = {'n_estimators': hp.choice('n_estimators', [50, 100, 150, 200, 250, 300]),
+         'max_depth': hp.choice('max_depth', [None, 10, 20, 30]),
+         'min_samples_split': hp.choice('min_samples_split', [2, 7, 12, 17, 22]),
+         'min_samples_leaf': hp.choice('min_samples_leaf', [2, 6, 10]),
+
+         'preprocessor': hp.choice(
+             'preprocessor', ['StandardScaler', 'RobustScaler', 'Normalizer', 'MaxAbsScaler']),
+
+         'feature_extractor': hp.choice(
+             'feature_extractor', [
+                 {
+                     'type': 'pca',
+                     'n_components': hp.choice('n_components', [10, 50, 75, 110]),
+                     'whiten': hp.choice('whiten', [True, False])
+                 }, {
+                     'type': 'RFE',
+                     'n_features_to_select': hp.choice(
+                         'n_features_to_select', [10, 50, 75, 110]),
+                     'step': 10,
+                 }, {
+                     'type': 'SelectKBest',
+                     'k': hp.choice(
+                         'k', [10, 50, 75, 110]),
+                 }]),
+         }
+
+@timeout_decorator.timeout(5)
+def model_from_param(params, X, y):
+
+    import numpy as np
+    np.random.seed(0)
+
+    from hyperopt import STATUS_OK
+
+    from sklearn.ensemble import RandomForestRegressor
+
+    from sklearn.pipeline import Pipeline
+    from sklearn.preprocessing import (StandardScaler, Normalizer, RobustScaler,
+                                       MaxAbsScaler)
+    from sklearn.impute import SimpleImputer
+    from sklearn.decomposition import PCA
+    from sklearn.metrics import mean_absolute_error
+    from sklearn.model_selection import cross_val_predict
+
+    from sklearn.feature_selection import VarianceThreshold, RFE, SelectKBest, f_regression
+    from sklearn import svm
+
+    print('Params testing: ', params)
+    
+    rf_model = RandomForestRegressor(n_estimators=params['n_estimators'],
+                                     max_depth=params['max_depth'],
+                                     min_samples_split=params['min_samples_split'],
+                                     min_samples_leaf=params['min_samples_leaf'])
+    
+    #print("RF computation : done ")
+
+    imputer = SimpleImputer(missing_values=np.nan, strategy='constant')
+    var_filter = VarianceThreshold()
+    #print("imputer and var_filter : done ")
+
+    preprocessor_dict = {
+        'StandardScaler': StandardScaler,
+        'MaxAbsScaler': MaxAbsScaler,
+        'Normalizer': Normalizer,
+        'RobustScaler': RobustScaler,
+    }
+
+    scaler = preprocessor_dict[params['preprocessor']]()
+
+    if params['feature_extractor']['type'] == 'pca':
+        opts = dict(params['feature_extractor'])
+        del opts['type']
+        feature_extraction = PCA(**opts)
+
+    elif params['feature_extractor']['type'] == 'RFE':
+        opts = dict(params['feature_extractor'])
+        del opts['type']
+        svr = svm.SVR(kernel='linear')
+        feature_extraction = RFE(estimator=svr, **opts)
+
+    elif params['feature_extractor']['type'] == 'SelectKBest':
+        opts = dict(params['feature_extractor'])
+        del opts['type']
+        feature_extraction = SelectKBest(score_func=f_regression, **opts)
+    
+    #print("Feature extractor : done ")
+
+    model = Pipeline(steps=[
+        ('imputer', imputer),
+        ('filter', var_filter),
+        ('scaler', scaler),
+        ('feature_extraction', feature_extraction),
+        ('rf_model', rf_model)
+    ])
+    
+    #print("pipeline : done ")
+    #labels = LabelEncoder().fit_transform(y.Type)
+
+    #n_repeats = 3
+    #acc = np.zeros(n_repeats)
+
+    #for i in range(n_repeats):
+        #print(i)
+    y_cv_predict = cross_val_predict(model, X.values, y.values, cv=3)
+    mae = mean_absolute_error(y.values, y_cv_predict)
+
+    #print("cross validate. : done ")
+
+    return {'loss': mae,
+            'loss_variance': mae.std()**2,
+            'status': STATUS_OK,
+            'params': params,
+            'y_pred': y_cv_predict} 
+
+    #print('ALL done')
+
+
+final_train = pd.read_csv('/home/onyxia/work/aml_project/claire/final_train.csv')
+y_train = pd.read_csv('/home/onyxia/work/aml_project/claire/y_train.csv')['y']
+
+
+from hyperopt.mongoexp import Trials
+trials = Trials()
+
+best = fmin(
+    fn=partial(model_from_param, X=final_train, y=y_train),
+    space=space,
+    algo=tpe.suggest,
+    max_evals=5,
+    trials=trials)
+
+print("Best estimator:", best)
