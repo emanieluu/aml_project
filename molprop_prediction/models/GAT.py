d1af624b33978f1df191f32ba927078a2ed2d88a
emanieluu <emanieluu@gmail.com>
2024-01-19 13:53:41 +0100

    cleaning

--- molprop_prediction/models/GAT.py
+++ molprop_prediction/models/GAT.py
@@ -8,19 +8,23 @@ class GATGraphRegressor(torch.nn.Module):
     def __init__(self, num_node_features, hidden_dim, out_features):
         super(GATGraphRegressor, self).__init__()
         self.conv1 = GATConv(num_node_features, hidden_dim)
+        self.bn1 = torch.nn.BatchNorm1d(hidden_dim)
         self.conv2 = GATConv(hidden_dim, hidden_dim)
+        self.bn2 = torch.nn.BatchNorm1d(hidden_dim)
         self.conv3 = GATConv(hidden_dim, hidden_dim)
+        self.bn3 = torch.nn.BatchNorm1d(hidden_dim)
         self.conv4 = GATConv(hidden_dim, hidden_dim)
+        self.bn4 = torch.nn.BatchNorm1d(hidden_dim)
         self.fc = torch.nn.Linear(hidden_dim, out_features)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        x = F.relu(self.conv1(x, edge_index))
+        x = F.relu(self.bn1(self.conv1(x, edge_index)))
         x = F.dropout(x, training=self.training)
-        x = F.relu(self.conv2(x, edge_index))
+        x = F.relu(self.bn2(self.conv2(x, edge_index)))
         x = F.dropout(x, training=self.training)
-        x = F.relu(self.conv3(x, edge_index))
+        x = F.relu(self.bn3(self.conv3(x, edge_index)))
         x = F.dropout(x, training=self.training)
-        x = F.relu(self.conv4(x, edge_index))
+        x = F.relu(self.bn4(self.conv4(x, edge_index)))
         x = global_mean_pool(x, data.batch)
         return self.fc(x)
